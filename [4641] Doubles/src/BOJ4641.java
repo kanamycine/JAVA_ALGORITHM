5faf0eb6db268eb2384be586fab773bed14ff4d9
kanamycine <jinyeong5320@gmail.com>
2022-01-01 01:52:34 +0900

    feat(4641) : Doubles

---
+++ [4641] Doubles/src/BOJ4641.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ4641 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true){
+			int res = 0;
+			String stringInput = sc.nextLine();
+			if (stringInput.equals("-1")){
+				return ;
+			}
+			List<String> lst = Arrays.asList(stringInput.split(" "));
+			List<Integer> intLst = new ArrayList<>();
+			for (int i = 0; i < lst.size(); i++) {
+				intLst.add(Integer.parseInt(lst.get(i)));
+			}
+
+			intLst.remove(intLst.size()-1);
+			Collections.sort(intLst);
+			for (int i = 0; i < intLst.size(); i++) {
+				if(intLst.contains(intLst.get(i) * 2)){
+					res += 1;
+				}
+			}
+			System.out.println(res);
+		}
+	}
+}
