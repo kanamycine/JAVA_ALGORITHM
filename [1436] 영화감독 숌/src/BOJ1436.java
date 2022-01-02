04d6cdc4fda965e980ac6609cab26d1a4fa58527
kanamycine <jinyeong5320@gmail.com>
2022-01-02 22:50:26 +0900

    feat(1436) : 영화감독 숌

---
+++ [1436] 영화감독 숌/src/BOJ1436.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ1436 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 1;
+		int i = 666;
+		while(cnt != n+1){
+			String str = Integer.toString(i);
+			if(str.contains("666")) {
+				cnt += 1;
+			}
+			i++;
+		}
+		System.out.println(i -1);
+	}
+}
