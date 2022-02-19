6051d306b610285769ff789708ebbb93cf7d22b8
kanamycine <jinyeong5320@gmail.com>
2022-02-19 15:38:06 +0900

    feat : 1021

---
+++ [1021] 회전하는 큐/src/BOJ1021.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class BOJ1021 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		LinkedList<Integer> dq = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			dq.offerLast(i);
+		}
+
+		int[] arr = new int[m];
+
+		for (int i = 0; i < m; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int cnt = 0;
+
+		for (int i = 0; i < m; i++) {
+			int t_idx = dq.indexOf(arr[i]);
+			int h_idx = 0;
+
+			if (dq.size() % 2 == 0) {
+				h_idx = dq.size() / 2 - 1;
+			} else
+				h_idx = dq.size() / 2;
+
+			if (t_idx <= h_idx) {
+				for (int j = 0; j < t_idx; j++) {
+					int tmp = dq.pollFirst();
+					dq.offerLast(tmp);
+					cnt++;
+				}
+			} else {
+				for (int j = 0; j < dq.size() - t_idx; j++) {
+					int tmp = dq.pollLast();
+					dq.offerFirst(tmp);
+					cnt++;
+				}
+			}
+			dq.pollFirst();
+		}
+
+		System.out.println(cnt);
+
+	}
+}
