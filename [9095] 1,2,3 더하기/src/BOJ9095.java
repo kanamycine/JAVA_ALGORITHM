b1bfd07d4dda3ef387bf4f39357ca990f8049687
kanamycine <jinyeong5320@gmail.com>
2022-03-04 17:50:40 +0900

    feat : 9095

---
+++ [9095] 1,2,3 더하기/src/BOJ9095.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BOJ9095 {
+	private static int [] result;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int t = sc.nextInt();
+		result = new int [t];
+		for (int i = 0; i < t; i++) {
+			result[i] = 0;
+			int n = sc.nextInt();
+			backtracking(n, i);
+			System.out.println(result[i]);
+		}
+	}
+
+	static void backtracking(int n, int order){
+		if (n == 0){
+			result[order]++;
+		}
+
+		for (int i = 1; i <= 3; i++) {
+			int chk = n-i;
+			if(chk>=0){
+				backtracking(n-i, order);
+			}
+		}
+	}
+}
