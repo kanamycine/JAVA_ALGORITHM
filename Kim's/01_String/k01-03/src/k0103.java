7402c9387c766841f4107d0601aa8bb40ae292fa
kanamycine <jinyeong5320@gmail.com>
2022-03-10 19:37:48 +0900

    feat : k0103

---
+++ Kim's/01_String/k01-03/src/k0103.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class k0103 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+		String [] arr = input.split(" ");
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			max = Math.max(arr[i].length(), max);
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i].length() == max){
+				System.out.println(arr[i]);
+				return;
+			}
+		}
+	}
+}
