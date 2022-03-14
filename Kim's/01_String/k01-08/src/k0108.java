af9a9464dbaa2d3b224b0577525e36ff41d238a9
kanamycine <jinyeong5320@gmail.com>
2022-03-15 00:52:06 +0900

    feat : k0108

---
+++ Kim's/01_String/k01-08/src/k0108.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class k0108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+		input = input.replaceAll("[^a-zA-Z0-9]", "");
+
+		StringBuilder sb = new StringBuilder(input);
+		if(input.equalsIgnoreCase(sb.reverse().toString())){
+			System.out.println("YES");
+		}
+		else
+			System.out.println("NO");
+	}
+}
