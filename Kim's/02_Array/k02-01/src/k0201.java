49da306a745a3bb248cc4b29bc0557205acb6a3a
kanamycine <jinyeong5320@gmail.com>
2022-03-19 16:39:26 +0900

    feat : k0201

---
+++ Kim's/02_Array/k02-01/src/k0201.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class k0201 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.nextLine());
+
+		String input = sc.nextLine();
+		String [] tmp = input.split(" ");
+		int [] arr = new int[n];
+		for (int i = 0; i < tmp.length; i++) {
+			arr[i] = Integer.parseInt(tmp[i]);
+		}
+
+		System.out.print(arr[0] + " ");
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] < arr[i+1]){
+				System.out.print(arr[i + 1] + " ");
+			}
+		}
+	}
+}
