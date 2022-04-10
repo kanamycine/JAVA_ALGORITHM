720c4926cee80868b22c1f99224965d8a424b4ee
kanamycine <jinyeong5320@gmail.com>
2022-04-10 15:33:33 +0900

    feat : k0603

---
+++ k06-03/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int [] arr = new int [n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		for (int i = 1; i < n; i++) {
+			int target = arr[i];
+			int j = i - 1;
+
+			while(j >= 0 && target < arr[j]){
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = target;
+		}
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+}
