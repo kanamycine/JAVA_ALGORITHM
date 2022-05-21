eccfe091e6055d425e971051c6c8124fb84be464
kanamycine <jinyeong5320@gmail.com>
2022-05-21 23:02:35 +0900

    feat : snowhotel

--- BitComputer/src/Main.java
+++ BitComputer/src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-	public static void main(String[] args) {
+import java.util.HashMap;
 
+public class Main {
+	HashMap<Long, Long> map = new HashMap<>();
+	public long [] solution(long k, long [] room_number){
+		long [] result = new long [room_number.length];
+
+		for (int i = 0; i < room_number.length; i++) {
+			result[i] = findVacant(room_number[i]);
+		}
+		return result;
+	}
+
+	public long findVacant(long targetRoomNumber){
+		if(!map.containsKey(targetRoomNumber)){
+			map.put(targetRoomNumber, targetRoomNumber + 1);
+			return targetRoomNumber;
+		}
+
+		long nextRoom = map.get(targetRoomNumber);
+		long vacantRoom = findVacant(nextRoom);
+		map.put(targetRoomNumber, vacantRoom);
+		return vacantRoom;
 	}
 }
